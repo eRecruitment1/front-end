e4e374c1518a5a5420e253ddd9ce9359c90039f4
unknown <quanghuyhihi128@gmail.com>
2022-09-22 11:10:06 +0700

    Fix Navbar

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <>
+            <div className='bg-black'>
+                <h1>WeHrCompany</h1>
+            </div>
+        </>
+    );
+}
+
+export default Footer
