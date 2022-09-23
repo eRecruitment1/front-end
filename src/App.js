315a37155370f15d3a62fc16246f0a0ab0dd6b5d
unknown <quanghuyhihi128@gmail.com>
2022-09-23 17:27:49 +0700

    RCRTMNT1-48 - Handle Firebase & Google Authentication

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom'
+import {AuthContextProvider} from './context/AuthContext'
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
-
+import Login from './pages/Login'
+import Account from './pages/Account'
 function App() {
   return (
     <>
-        <Navbar />
-        <Home />
+    <AuthContextProvider>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/account" element={<Account />} />
+      </Routes>
+    </AuthContextProvider>
     </>
   );
 }
