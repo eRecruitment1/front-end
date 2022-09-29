f1454a3262ef27cdb7e8dae68200cf8c25725294
mytran <tranntmse150602@fpt.edu.vn>
2022-09-29 20:58:56 +0700

    add about us

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {AuthContextProvider} from './context/AuthContext'
 import Home from './pages/Home';
 import LoginWithGoogle from './pages/Login/LoginWithGoogle'
 import Account from './pages/Account'
+import About from './pages/About'
 function App() {
   return (
     <>
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginWithGoogle />} />
         <Route path="/account" element={<Account />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </AuthContextProvider>
     </>
