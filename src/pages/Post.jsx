0415095e6796ddbe4bbec29a3feeddb19228fec1
unknown <quanghuyhihi128@gmail.com>
2022-09-30 11:58:04 +0700

    Merge add2

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
+import Navbar from '../components/Header/Navbar'
 import Footer from '../components/Footer'
 const Post = () => {
     return (
