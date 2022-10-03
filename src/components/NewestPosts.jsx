beda86d409e556c8911cf4f25afb2d594a6183e2
mytran <tranntmse150602@fpt.edu.vn>
2022-10-04 00:26:26 +0700

    fix img

--- src/components/NewestPosts.jsx
+++ src/components/NewestPosts.jsx
@@ -8,7 +8,7 @@ const NewestPosts = () => {
                 <h1 className="text-3xl font-semibold text-gray-800 capitalize lg:text-4xl dark:text-white">New Posts</h1>
                 <div className="grid grid-cols-1 gap-8 mt-8 md:mt-16 md:grid-cols-2">
                     <div className="lg:flex transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300">
-                        <img className="object-fit: contain" src="https://bom.so/RzuVpd" alt="" />
+                            <img className="object-cover w-full h-56 rounded-lg lg:w-56" src="https://firebasestorage.googleapis.com/v0/b/erecruitment-71104.appspot.com/o/2497976-1.jpg?alt=media&token=b41171f9-69ac-4c22-b829-420852d86979" alt="" />
                         <div className="flex flex-col justify-between py-6 lg:mx-6">
                             <a href="/" className="text-xl font-semibold text-gray-800 hover:underline dark:text-white ">
                                 Java Software Architect
@@ -23,7 +23,7 @@ const NewestPosts = () => {
                         </div>
                     </div>
                     <div className="lg:flex">
-                        <img className="object-cover w-full h-56 rounded-lg lg:w-56" src="https://bom.so/x0Ptma" alt="" />
+                        <img className="object-cover w-full h-56 rounded-lg lg:w-56" src="https://firebasestorage.googleapis.com/v0/b/erecruitment-71104.appspot.com/o/2497976-2.jpg?alt=media&token=c9da3797-d2c0-468b-8118-432c5d03ccae" alt="" />
                         <div className="flex flex-col justify-between py-6 lg:mx-6">
                             <a href="/" className="text-xl font-semibold text-gray-800 hover:underline dark:text-white ">
                                 Senior Engineering Manager
@@ -35,7 +35,7 @@ const NewestPosts = () => {
                         </div>
                     </div>
                     <div className="lg:flex">
-                        <img className="object-cover w-full h-56 rounded-lg lg:w-56" src="https://bom.so/g6mbTA" alt="" />
+                        <img className="object-cover w-full h-56 rounded-lg lg:w-56" src="https://firebasestorage.googleapis.com/v0/b/erecruitment-71104.appspot.com/o/2497976-3.jpg?alt=media&token=128c4314-387d-4269-95bf-983181eb9b6f" alt="" />
                         <div className="flex flex-col justify-between py-6 lg:mx-6">
                             <a href="/" className="text-xl font-semibold text-gray-800 hover:underline dark:text-white ">
                                 Engineering Manager
@@ -47,7 +47,7 @@ const NewestPosts = () => {
                         </div>
                     </div>
                     <div className="lg:flex">
-                        <img className="object-cover w-full h-56 rounded-lg lg:w-56" src="https://bom.so/ZKP3Fr" alt="" />
+                        <img className="object-cover w-full h-56 rounded-lg lg:w-56" src="https://firebasestorage.googleapis.com/v0/b/erecruitment-71104.appspot.com/o/2497976-4.jpg?alt=media&token=7b1fa3b3-37bb-4479-873d-946300d4dea7" alt="" />
                         <div className="flex flex-col justify-between py-6 lg:mx-6">
                             <a href="/" className="text-xl font-semibold text-gray-800 hover:underline dark:text-white ">
                                 Senior .NET Software Engineer
@@ -59,7 +59,7 @@ const NewestPosts = () => {
                         </div>
                     </div>
                     <div className="lg:flex">
-                        <img className="object-cover w-full h-56 rounded-lg lg:w-56" src="https://bom.so/XPqztb" alt="" />
+                        <img className="object-cover w-full h-56 rounded-lg lg:w-56" src="https://firebasestorage.googleapis.com/v0/b/erecruitment-71104.appspot.com/o/2497976-6.jpg?alt=media&token=97765dce-e3c2-4511-bb3b-9c7c625f1012" alt="" />
                         <div className="flex flex-col justify-between py-6 lg:mx-6">
                             <a href="/" className="text-xl font-semibold text-gray-800 hover:underline dark:text-white ">
                                 .NET Software Engineer Intern
@@ -71,7 +71,7 @@ const NewestPosts = () => {
                         </div>
                     </div>
                     <div className="lg:flex">
-                        <img className="object-cover w-full h-56 rounded-lg lg:w-56" src="https://bom.so/WZ6n5k" alt="" />
+                        <img className="object-cover w-full h-56 rounded-lg lg:w-56" src="https://firebasestorage.googleapis.com/v0/b/erecruitment-71104.appspot.com/o/2497976-5.jpg?alt=media&token=1a94a21f-54c5-48fd-869f-74542d30829a" alt="" />
                         <div className="flex flex-col justify-between py-6 lg:mx-6">
                             <a href="/" className="text-xl font-semibold text-gray-800 hover:underline dark:text-white ">
                                 Senior Java Software Engineer
